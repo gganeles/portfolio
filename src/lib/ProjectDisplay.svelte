e83df2be4d5b7d73de4377dc41aedb135081cc94
gganeles <gabriel.ganeles@gmail.com>
2024-04-08 00:18:26 +0300

    Added Content template

---
+++ src/lib/ProjectDisplay.svelte
@@ -0,0 +1,32 @@
+<script>
+    export let title = "";
+    export let src = '/images/e44d5f00-6bed-4075-bb5c-c2f91fbe77b4.jpeg';
+    export let desc;
+</script>
+
+<div class="group w-full h-full relative hover:cursor-pointer">
+    <!-- <div class="picture w-full h-full">
+         text
+     </div>-->
+    <div class="absolute w-full h-full group-hover:bg-black group-hover:bg-opacity-50 transition">
+
+    </div>
+    <div class="absolute bottom-0 p-1 text-pretty text-slate-200 bg-black bg-opacity-50 w-full">
+        <h3>
+            {title}
+        </h3>
+        {#if $$props.desc}
+            <p class="text-sm">
+                {desc}
+            </p>
+        {/if}
+    </div>
+    <img {src} alt="{title}"/>
+</div>
+
+<style>
+    .picture {
+        background-image: var(--src);
+        background-size: cover;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,39 @@
+<script lang="ts">
+    import ProjectDisplay from "$lib/ProjectDisplay.svelte";
 
+    const generateProject = (title:string,src?:string, desc = "") => {
+        return {src, title, desc}
+    };
 
+    let projects = [
+        generateProject("ShabBOT:","/images/93b13e09-5dad-4fab-b131-a5488a0df60b.jpeg", "Shabbat Whatsapp Assistant"),
+        generateProject("Formula Technion Website","/images/Screenshot 2024-04-07 235706.png"),
+        generateProject("Formula Student PCB Design"),
+        generateProject("Parachute Mechanism"),
+        generateProject("CERN Intern"),
+        generateProject("sEEG Data Analysis Pipeline"),
+        generateProject("Robotic Gripper Design"),
+    ]
+</script>
 
-<h1>
-    Gabriel Ganeles
-</h1>
-<div>
-    Projects:
+<div class="p-4 pt-10">
+    <h1 class="text-7xl pb-2">
+        Gabriel Ganeles
+    </h1>
+    <h2 class="py-2">
+        Age: 22
+    </h2>
+    <div class="pb-2">
+        Skills: software development, mechanical design, electrical design
+    </div>
+    <div>
+        <div class="pb-2">
+            Projects:
+        </div>
+        <div class="grid grid-cols-3 gap-1">
+            {#each projects as proj}
+                <ProjectDisplay src={proj.src} title={proj.title} desc={proj.desc}/>
+            {/each}
+        </div>
+    </div>
 </div>
